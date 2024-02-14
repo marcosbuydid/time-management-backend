fb550f67cd5b0906154b78fea40cf44bb3b3373d
marcos <marcosbuydid@gmail.com>
2024-02-13 21:15:07 -0300

    add  user validations and password encryption on db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,84 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
 
-const createUser = (req, res = response) => {
-
-    const { name, email, password } = req.body;
-
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password
-    })
-}
-
-const login = (req, res = response) => {
+const createUser = async (req, res = response) => {
 
     const { email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    })
+    try {
+        let user = await User.findOne({ email });
+
+        if (user !== null) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User already defined with this email'
+            });
+        }
+
+        user = new User(req.body);
+
+        //encrypt password
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(password, salt);
+
+        //save user on database
+        await user.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact database administrator'
+        });
+    }
+}
+
+const login = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (user === null) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User or Password are invalid'
+            });
+        }
+
+        //check if passwords match
+        const validatePassword = bcrypt.compareSync(password, user.password);
+
+        //response is true or false
+        if (!validatePassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User or Password are invalid'
+            });
+        }
+
+        //generate JWT
+
+        res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact database administrator'
+        });
+    }
 }
 
 const refreshToken = (req, res = response) => {
